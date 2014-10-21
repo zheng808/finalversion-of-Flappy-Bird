5797711bcaf359b32491ba9a22a9da61083d850a
zheng808 <lizheng808@gmail.com>
2014-10-20 18:28:38 -0700

    Update GameViewController.h

--- GameViewController.h
+++ GameViewController.h
@@ -2,22 +2,41 @@
 //  GameViewController.h
 //  Flappy bird
 //
-//  Created by lizheng on 2014-04-30.
+//  Game.h
+//  finalversion of Flappy Bird
+//
+//  Created by lizheng on 2014-05-01.
 //  Copyright (c) 2014 lizheng. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-
-
 int BirdFlight;
-@interface GameViewController : UIViewController
+int RandomTopTunnelPosition;
+int RandomBottomTunnelPosition;
+int ScoreNumber;
+NSInteger HighScoreNumber;
+@interface Game : UIViewController //class 
+
 {
-    IBOutlet UIView *Bird;
+    IBOutlet UIImageView *Bird;
     IBOutlet UIButton *StartGame;
+    IBOutlet UIImageView *TunnelTop;
+    IBOutlet UIImageView *TunnelBottom;
+    IBOutlet UIImageView *Top;
+    IBOutlet UIImageView *Bottom;
+    IBOutlet UIButton *Exist;
+    IBOutlet UILabel *ScoreLabel;
+    
+
+    NSTimer *TunnelMovement;
     NSTimer *BirdMovement;
     
 }
--(IBAction)StartGame:(id)sender;
+-(IBAction)StartGame:(id)sender; //declaretion method
 -(void)BirdMoving;
-
+-(void)TunnelMoving;
+-(void)PlaceTunnel;
+-(void)Score;
+-(void)GameOver;
 @end
+
